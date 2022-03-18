7fd4cc42bd4249528256abbab650c575d4b36537
Hugo Dutra <hugo.dutra@hotmail.com>
2022-03-18 09:30:25 -0300

    button ajustes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,16 @@
 import './Button.css';
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';   
 
 const Button = props => {
+  const {label} = props;
   return (
     <div>Button</div>
   )
 }
 
-Button.propTypes = {};
+Button.propTypes = {
+  label:PropTypes.string.isRequired
+};
 
 export default Button;
